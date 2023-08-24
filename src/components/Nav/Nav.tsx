0d656674af9c099130ed4972a301db14c8f4c517
NDenheim <nicole.denheim@hotmail.co.uk>
2023-08-24 18:59:35 +0100

    Added random button plus styling

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,6 +1,7 @@
-import { FormEventHandler } from "react";
+import { FormEventHandler, useState } from "react";
 import FiltersList from "../FiltersList/FiltersList";
 import SearchBar from "../SearchBar/SearchBar";
+import { Link } from "react-router-dom";
 import "./Nav.scss";
 
 type NavProps = {
@@ -9,13 +10,46 @@ type NavProps = {
 };
 
 const Nav = ({ handleSearch, handleFilter }: NavProps) => {
+  let randomNumber;
+
+  const numberGenerator = () => {
+    randomNumber = Math.floor(Math.random() * 325) + 1;
+    console.log(randomNumber);
+    return randomNumber;
+  };
+
   return (
     <nav className="nav">
-      <h2 className="nav__heading">BEER HUNTER</h2>
+      <Link to={`/`} style={{ textDecoration: "none" }}>
+        <h2 className="nav__heading">BEER HUNTER</h2>
+      </Link>
       <SearchBar handleSearch={handleSearch} />
       <FiltersList onChange={handleFilter} />
+      <Link to={`/${randomNumber}`}>
+        <button className="nav__button" onClick={numberGenerator}>
+          Random
+        </button>
+      </Link>
     </nav>
   );
 };
 
 export default Nav;
+
+// let randomNumber;
+
+//   const numberGenerator = () => {
+//     randomNumber = Math.floor(Math.random() * 325) + 1;
+//     console.log(randomNumber);
+//     return randomNumber;
+//   };
+
+{
+  /* <Link to={`/${randomNumber}`}>
+<button onClick={numberGenerator}>Random</button>
+</Link> */
+}
+
+//   <Link to={`/${Math.floor(Math.random() * 325) + 1}`}>
+//         <button onClick={numberGenerator}>Random</button>
+//       </Link>
